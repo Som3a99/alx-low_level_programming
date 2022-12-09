4a55583a6b84378673e254952dcb8fdf8c1f8183
mohamedIsmail <mohamed_emohamed@outlook.com>
2022-12-09 16:11:47 +0200

    alphabet part 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entery point
+ *Return: return (0)
+*/
+
+int main(void)
+{
+for (char i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
