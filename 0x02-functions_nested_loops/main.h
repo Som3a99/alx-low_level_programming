c99231a66e529e362f3df329e550e574b3cd256c
mohamedIsmail <mohamed_emohamed@outlook.com>
2022-12-13 23:40:31 +0200

    very diffcult

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,12 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 /**
 *File:main.h
 *Author:Mohamed Ismail
 *Desc:header file containing functions declaration
 */
 
+void print_alphabet(void);
 int _putchar(char);
+
+#endif /*MAIN_H*/
