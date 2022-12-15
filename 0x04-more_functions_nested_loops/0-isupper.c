a328263baf62f81414444ffdde63db32ff658202
mohamedIsmail <mohamed_emohamed@outlook.com>
2022-12-15 13:36:25 +0200

    betty is diffcult women

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
 /**
- * int _isupper(int c); - a function that checks for uppercase character.
- * RETURN: Returns 1 if c is uppercase Returns 0 otherwise
+ *_isupper - a function that checks for uppercase character.
+ *@c: tested character
+ *Return: Returns 1 if c is uppercase Returns 0 otherwise
 */
 
 int _isupper(int c)
