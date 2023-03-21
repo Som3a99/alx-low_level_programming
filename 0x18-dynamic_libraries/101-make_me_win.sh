8e5bdbb544af6770f88d6f6f92c7e77464911404
mohamedIsmail <mohamed_emohamed@outlook.com>
2023-03-21 03:10:51 +0200

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Som3a99/alx-low_level_programming/tree/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/Som3a99/alx-low_level_programming/tree/master/0x18-dynamic_libraries/random_check.so
+export LD_PRELOAD=/tmp/random_check.so
